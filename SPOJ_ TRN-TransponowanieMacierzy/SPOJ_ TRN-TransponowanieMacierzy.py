f3ab7382beb61d6698efcb7a162834ccd8d0815c
APozorska <falkiewicz.aleksandra@gmail.com>
2024-10-09 13:24:37 +0200

    Update SPOJ_ TRN-TransponowanieMacierzy.py

--- SPOJ_ TRN-TransponowanieMacierzy/SPOJ_ TRN-TransponowanieMacierzy.py
+++ SPOJ_ TRN-TransponowanieMacierzy/SPOJ_ TRN-TransponowanieMacierzy.py
@@ -1,5 +1,18 @@
 # SPOJ | TRN - Transponowanie macierzy | https://pl.spoj.com/problems/TRN/
 # Author: Pozorska Aleksandra
+"""
+This is the program that transposes a given matrix.
+Input:
+4 3
+1 2 5
+4 3 3
+3 4 9
+8 7 7
+Output:
+1 4 3 8
+2 3 4 7
+5 3 9 7
+"""
 
 m, n = map(int, input().split())
 matrix = []
